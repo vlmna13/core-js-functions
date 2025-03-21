c79c0ab59f11a605cdfb5f70eef196816507dea4
vlmna13 <vlmna13@gmail.com>
2025-03-21 23:20:11 +0300

    feat: add sollutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -189,8 +189,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function ar(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -210,8 +212,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom;
+  return () => {
+    const result = value;
+    value += 1;
+    return result;
+  };
 }
 
 module.exports = {
